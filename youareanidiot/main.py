e43dd3e22493a364b1cb6b23cb106e978ed7b0f6
wsn0672 <yutomonceau@gmail.com>
2024-12-01 22:08:24 +0900

    youareanidiot!
    説明なんてねぇよ

---
+++ youareanidiot/main.py
@@ -0,0 +1,47 @@
+# uncompyle6 version 3.9.2
+# Python bytecode version base 3.8.0 (3413)
+# Decompiled from: Python 3.12.6 (v3.12.6:a4a2d2b0d85, Sep  6 2024, 16:08:03) [Clang 13.0.0 (clang-1300.0.29.30)]
+# Embedded file name: crazyrockerror2.py
+from playsound import *
+import webbrowser
+import tkinter as tk
+from tkinter import messagebox
+from random import *
+import time
+
+def aori():
+    res = messagebox.askyesno("質問です", "あなたのパソコンをウイルス感染させてもいいですか？", icon="question")
+
+def osoi():
+    res = messagebox.showinfo("残念", "でも残念、もう手遅れ。")
+
+def hello():
+    res = messagebox.showinfo("💀💀💀", "あなたのコンピューターはウイルスに感染しています", icon='warning')
+
+def helloo():
+    res = messagebox.showinfo("you are an idiot", "存分にウイルスをお楽しみください。", detail='あ、この警告は消せませんよ(笑)')
+
+playsound(".//Your Love Is My Drug 8bit Slowed.wav", False)
+
+root = tk.Tk()
+root.withdraw()
+aori()
+
+root = tk.Tk()
+root.withdraw()
+osoi()
+
+root = tk.Tk()
+root.withdraw()
+hello()
+
+root = tk.Tk()
+root.withdraw()
+helloo()
+
+while True:
+    print("youareanidiot")
+    print("あなたは馬鹿です")
+    webbrowser.open("https://youareanidiot.cc/")
+
+# okay decompiling crazyrockerror2.pyc 
